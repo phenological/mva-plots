1564d46ef490b0dcb2ed967ae861b0cf7afe7221
reika2 <39753352+reika2@users.noreply.github.com>
2023-07-21 15:12:55 +0800

    roi parameters added

--- R/PlotLoadSpec.R
+++ R/PlotLoadSpec.R
@@ -8,6 +8,7 @@
 #'                            ropls::opls()
 #' @param PC is the number of components that you want to plot the loadings of
 #' Default is 1 and only require to change when plotting PCA or PLS model
+#' @param  roi is the region of interest (default is from 0.5 to 9.5 ppm)
 #' @return plot of the loadings
 #' @import scales
 #' @import colorRamps
@@ -26,28 +27,37 @@
 #'
 #' PlotLoadSpec(model = prcomp_pca_model)
 
-PlotLoadSpec<-function(model,PC = 1,option = list()){
+PlotLoadSpec<-function(model,PC = 1,roi = c(0.5,9.5),option = list()){
   if(class(model)[1]=="prcomp"){
     df <- data.frame(-model$rotation)
     res<-summary(model)
     res<-round(c(res$importance['Proportion of Variance',1:ncol(df)])*100,1)
     x<-as.numeric(rownames(df))
-    cen<-data.frame(model$center)
+    idx<-which(x>roi[1] & x<roi[2])
+    df<-df[idx,]
+    x<-x[idx]
+    cen<-data.frame(model$center[idx])
     method = "PCA"
   }
-  if(class(model)[1]=="list"){  # need to change this, for now, PCA() using library(prcomp) return list of 2
+  if(class(model)[1]=="list" & names(model)[1]=="data"){  # need to change this, for now, PCA() using library(prcomp) return list of 2
     res<-model$data$pcSum$`Proportion of Variance`
     df<-as.data.frame(-model$data$loadings)
     x<-as.numeric(rownames(df))
-    cen<-data.frame(model$data$center)
+    idx<-which(x>roi[1] & x<roi[2])
+    cen<-data.frame(model$data$center[idx])
+    df<-df[idx,]
+    x<-x[idx]
     method = "PCA"
   }
   if(class(model)[1]=="opls"){
-      res<-model@summaryDF
-      df<-data.frame(-model@loadingMN)
-      x<-as.numeric(rownames(df))
-      cen<--data.frame(model@weightMN)
-      method<-model@typeC
+    res<-model@summaryDF
+    df<-data.frame(-model@loadingMN)
+    x<-as.numeric(rownames(df))
+    idx<-which(x>roi[1] & x<roi[2])
+    df<-df[idx,]
+    x<-x[idx]
+    cen<--data.frame(model@weightMN[idx])
+    method<-model@typeC
   }
   if(!class(model)[1]%in%c("opls","list","prcomp")){
     stop("Check the Model class: must be prcomp,opls, or the results from PCA")
@@ -60,7 +70,7 @@ PlotLoadSpec<-function(model,PC = 1,option = list()){
   } else{
     stop("PC selected is larger than the dimension of the model")
   }
-
+  
   if(method=="PCA"){
     p1<-ggplot(df,aes(x = x, y = y,color = col))+geom_line()+
       scale_x_reverse(breaks = scales::breaks_pretty(n = 15))+
@@ -82,7 +92,7 @@ PlotLoadSpec<-function(model,PC = 1,option = list()){
            y = "") +
       theme_bw()
   }
-
+  
   return(p1)
-
+  
 }
