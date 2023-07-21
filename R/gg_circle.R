88a2131a9b75a08350766230f770a7ad68f280d2
Lucy <lucy@example.com>
2023-07-21 11:41:54 +0800

    change hotellings line to grey and dashed line

--- R/gg_circle.R
+++ R/gg_circle.R
@@ -1,11 +1,11 @@
-gg_circle <- function(rx, ry, xc, yc, color = "black", fill = NA, ...) {
+gg_circle <- function(rx, ry, xc, yc, color = "black", fill = NA, linetype = "dashed",...) {
   x <- xc + rx * cos(seq(0, pi, length.out = 100))
   ymax <- yc + ry * sin(seq(0, pi, length.out = 100))
   ymin <- yc + ry * sin(seq(0, -pi, length.out = 100))
   annotate(
     "ribbon",
     x = x, ymin = ymin, ymax = ymax,
-    color = color, fill = fill, ...
+    color = color, fill = fill, linetype = linetype, ...
   )
 }
 
