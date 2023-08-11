3087c2914150ca20b7a0a2c61ae949b2873cc952
Andres Bernal <otrobernal@gmail.com>
2023-08-11 16:48:23 +0800

    printing plot from stocsy without returning was inconvenient for ggplot2 "chaining". Now "plot" boolean argument controls wether plot (default) or raw data is returned.

--- R/stocsy.R
+++ R/stocsy.R
@@ -4,19 +4,28 @@
 #' @param Y numeric, spectra matrix, spectra in rows
 #' @param driver numeric, reference to compute correlation. If length(driver) matches the number of spectra, it is interpreted as the vector of observations of the driver variable. Otherwise, driver[1] is interpreted as the chemical shift coordinate of the driver.
 #' @param roi numeric, upper and lower limit of the Region Of Interest to be correlated. The whole spectra range by default.
-#' @param plot, boolean, whether to make a plot of the STOCSY or not
-#' @param breaks numeric, number of breaks in the x axis
-#' @return a data.frame with the correlations and covariances of Y with the driver and the corresponding ppm scale, restricted to the given roi. The driver shift is written to the column names. Return value is invisible.
+#' @param plot, boolean, whether to return the plot of the STOCSY (TRUE, default) or the numbers
+#' @param breaks numeric, number of breaks in the x axis or the STOCSY plot
+#' @return if plot=TRUE, a ggplot2 object. Otherwise, a data.frame with the correlations and covariances of Y with the driver and the corresponding ppm scale, restricted to the given roi. The "driver" (either a cshift or a vector of intensities) is stored as an attribute of the data.frame.
 #' @import ggplot2
 #' @importFrom colorRamps matlab.like2
 #' @export
 stocsy <- function(x, Y, driver, roi = range(x), plot = TRUE, breaks = 10){
-  #Parse driver and driver chemical shift, if relevant
+  #Parse the driver, which may be a chem. shift, into a driver vector
   if (is.numeric(driver)){
-    if (length(driver) != dim(Y)[1]){
-      cshift <- driver[1]
-      driver <- Y[,which.min(abs(x-cshift))]
-    } else cshift <- NULL
+    if (length(driver) == dim(Y)[1]){
+      driverV <- driver
+    } else{
+      driver <- driver[1]
+      if (driver >= min(x) & driver <= max(x)){
+        driverV <- Y[,which.min(abs(x-driver))]
+      }
+      else{
+        cat(crayon::red("mvaPlots::stocsy >>"
+                        ,"Driver shift is not in the spectral range\n"))
+        stop()
+      }
+    }
   }
   else{
     cat(crayon::red("mvaPlots::stocsy >>"
@@ -29,40 +38,39 @@ stocsy <- function(x, Y, driver, roi = range(x), plot = TRUE, breaks = 10){
   Y <- Y[,fi]
   #Compute covariance and correlation
   # cs <- which.min(abs(x-cshift))
-  cr <- abs(as.vector(cor(Y,driver)))
-  cv <- as.vector(cov(Y,driver))
+  cr <- abs(as.vector(cor(Y,driverV)))
+  cv <- as.vector(cov(Y,driverV))
   #Make return data.frame
-  df <- data.frame(ppm=x, corr=cr, covar=cv)
+  aDF <- data.frame(ppm=x, corr=cr, covar=cv)
+  attr(aDF,"driver") <- driver
   #Plot if required
   if (plot){
-    
-    print(plotStocsy(df, breaks=breaks, cshift=cshift))
+    return(plotStocsy(aDF, breaks=breaks))
   }
-  #Return result invisibly
-  invisible(df)
+  return(aDF)
 }
 
 #' Simple STOCSY visualization
 #' 
 #' @param stocsy, list, STOCSY results as returned by mvaPlots::stocsy()
-#' @param cshift, optional, the driver chemical shift. If provided it is inserted as a caption in the figure.
 #' @param breaks numeric, number of breaks in the x axis
-#' @return a ggplot of the STOCSY, with ppm in the horizontal axis, covariance in the vertical axis, and correlation in the colour scale
+#' @return a ggplot of the STOCSY, with ppm in the horizontal axis, covariance in the vertical axis, and correlation in the color scale
 #' @import ggplot2
 #' @importFrom colorRamps matlab.like2
 #' @export
-plotStocsy <- function(df, cshift=NULL, breaks=10){
-  p <- ggplot(df
+plotStocsy <- function(aDF, breaks=10){
+  driver <- attr(aDF,"driver")
+  p <- ggplot(aDF
               ,aes(x=ppm
                      ,y=covar
                      ,colour=corr)) + geom_line() + scale_x_reverse(n.breaks=breaks)
   p <- p + scale_colour_gradientn(colors = colorRamps::matlab.like2(10)
                                   , limits=c(0,1)
                                   , name="|R|")
-  if(!is.null(cshift)){
-    p <- p + labs(caption=paste("Driver c. shift:",cshift,"ppm"))
-    if (cshift >= min(df$ppm) & cshift <= max(df$ppm))
-      p <- p + geom_vline(xintercept = cshift,linetype = 2)
+  if(!is.null(driver) & length(driver) == 1){
+    p <- p + labs(caption=paste("Driver c. shift:",driver,"ppm"))
+    if (driver >= min(aDF$ppm) & driver <= max(aDF$ppm))
+      p <- p + geom_vline(xintercept = driver,linetype = 2)
   }
   return(p)
 }
