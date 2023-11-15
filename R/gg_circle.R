c60aa255d359c12f17129588355eabeb20a27237
Lucy <lucy@example.com>
2023-11-15 15:34:44 +0800

    attempt to remove warnings from plotscores generated by depreciated vector arithmetic

--- R/gg_circle.R
+++ R/gg_circle.R
@@ -1,11 +1,21 @@
-gg_circle <- function(rx, ry, xc, yc, color = "black", fill = NA, linetype = "dashed",...) {
+# gg_circle <- function(rx, ry, xc, yc, color = "black", fill = NA, linetype = "dashed",...) {
+#   x <- xc + rx * cos(seq(0, pi, length.out = 100))
+#   ymax <- yc + ry * sin(seq(0, pi, length.out = 100))
+#   ymin <- yc + ry * sin(seq(0, -pi, length.out = 100))
+#   annotate(
+#     "ribbon",
+#     x = x, ymin = ymin, ymax = ymax,
+#     color = color, fill = fill, linetype = linetype, ...
+#   )
+# }
+
+gg_circle <- function(rx, ry, xc, yc, color = "black", fill = NA, linetype = "dashed", ...) {
   x <- xc + rx * cos(seq(0, pi, length.out = 100))
   ymax <- yc + ry * sin(seq(0, pi, length.out = 100))
   ymin <- yc + ry * sin(seq(0, -pi, length.out = 100))
   annotate(
     "ribbon",
-    x = x, ymin = ymin, ymax = ymax,
+    x = c(x), ymin = c(ymin), ymax = c(ymax),
     color = color, fill = fill, linetype = linetype, ...
   )
 }
-
