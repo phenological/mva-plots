f0d3f254f5e80d2e5871cce59eff4fd627592801
Lucy <lucy@example.com>
2023-11-28 13:53:50 +0800

    ggcircle test update

--- tests/testthat/test-gg_circle.R
+++ tests/testthat/test-gg_circle.R
@@ -7,8 +7,12 @@ test_that("gg_circle produces the expected plot", {
   data <- matrix(rnorm(100), ncol = 5)
   #Do PCA
   result <- PCA(data)
+  n <- nrow(result[["data"]][["rawData"]])
+  ci <- 0.95
+  hotFisN <- (n - 1) * 2 * (n^2 - 1) / (n^2 * (n - 2)) * qf(ci, 2, n - 2)
+
   #make ggplot
-  p<- ggplot(data=a$data$pcdf, aes(x=a$data$pcdf$PC4, y=a$data$pcdf$PC5)) +
+  p<- ggplot(data=result$data$pcdf, aes(x=result$data$pcdf$PC4, y=result$data$pcdf$PC5)) +
     geom_point() +
     gg_circle( rx = sqrt(var(result$data$pcdf$PC4) * hotFisN),
                ry = sqrt(var(result$data$pcdf$PC5) * hotFisN),
