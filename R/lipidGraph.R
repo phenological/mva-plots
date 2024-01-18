f0915a177ab8b49bb92e507031172b23a68b6ed8
Lucy <lucy@example.com>
2024-01-18 10:03:02 +0800

    Delete pca_pred.png

--- R/lipidGraph.R
+++ R/lipidGraph.R
@@ -1,1 +1,290 @@
-#from covid19--> harvardC2-->analysis-->Phenomics_ManuscriptAnalysis... for lipid graph
+#' Lipid Graph
+#'
+#' Lipid plot of class vs side chain.
+#'
+#' @param stat The statistic you would like to use. Either log2 fold change "fc",
+#' Cliff's Delta "cd", correlation "corr" or "external". "fc" and "cd" are
+#' calculated automatically. "external" is an externally supplied statistic that
+#' can be, for example, a p-value from lmm and needs to be provided in the
+#' \code{optns}.
+#' @param external If stat is set to external, set this parameter in the
+#' \code{optns} list. Must be the same as the number of lipids.
+#' @param model A model from PCA or oplsda function, or a dataframe of lipids.
+#' @param color
+#' @param factor A parameter for the \code{optns} list. An object the same
+#' length as the data in the model supplied that must have at least 2 unique
+#' groups such as treatment and control. More than 2 is allowable.
+#' @param control A parameter for the \code{optns} list to set which group with
+#' the supplied factor you wish to set as the control for comparison to all
+#' other groups.
+#' @import stats
+#' @import ggplot2
+
+lipidGraph <- function(model, stat = "fc", optns = list()){
+
+  #check factors to see if any group is empty
+  if (any(is.na(optns$factor))) {
+    stop("One of your factors is NA, please change this before running lipidGraph")
+  }
+
+   if (!"control" %in% names(optns)) {
+    optns$control <- 1
+    #print warning
+    warning(paste0("No control specified in optns for factor. The first entry was set as the control"))
+   }
+
+  if("method" %in% names(optns)){
+    method = optns$method
+  }else{
+    method <- "bonferroni"
+  }
+
+  #PCA
+  if(is(model)[1] == "list"){
+    #id <- as.data.frame(colnames(model$data$rawData))
+    id <- colnames(model$data$rawData)
+    df <- model$data$rawData
+
+    # #empty df
+    # pvalUnadjusted <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+    # pvalRescaled <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+
+    df[,"factor"] <- as.numeric(relevel(as.factor(optns$factor), ref = optns$control))
+
+    #pcLoadings<-as.data.frame(abs(model$data$loadings[,PC]))
+  }
+
+  #opls
+  if(is(model)[1] == "opls"){
+    #id <- as.data.frame(colnames(as.data.frame(model@suppLs[["x"]])))
+    id <-names(model@suppLs[["x"]])
+    df <- as.data.frame(model@suppLs[["x"]])
+
+    # #empty df
+    # pvalUnadjusted <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+    # pvalRescaled <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+
+    df[,"factor"] <- as.numeric(relevel(as.factor(model@suppLs[["yMCN"]]), ref = optns$control))
+
+    #PC <-1
+    #pcLoadings<-as.data.frame(abs(model@loadingMN[,PC]))
+  }
+
+  #df
+  if(is(model)[1]== "data.frame"){
+    id <- colnames(model)
+    df <- model
+
+    # #empty df
+    # pvalUnadjusted <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+    # pvalRescaled <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+
+    df[,"factor"] <- as.numeric(relevel(as.factor(optns$factor), ref = optns$control))
+
+    #no loadings as it's not a model
+    #pcLoadings <- data.frame(matrix(0, nrow = ncol(model), ncol = 1))
+  }
+
+  unique_factors <- unique(df[,"factor"])
+
+
+  if(!("discretePalette" %in% names(optns))){
+    optns$discretePalette <- c("#66C2A5",
+                               "#FC8D62",
+                               "#8DA0CB",
+                               "#E78AC3",
+                               "#A6D854",
+                               "#FFD92F",
+                               "#E5C494",
+                               "#B3B3B3")
+  }
+
+########cliffs delta##########
+
+  if(stat == "cd"){
+    cd <- cliffsDelta(model = model, optns = optns)
+    statistic <- cd
+  }
+
+##########correlations between scaled + centered original data and scores######
+if(stat == "corr"){
+  if(is(model)[1] == "list"){
+    corr <- abs(t(as.data.frame(cor(model$data$scores[,PC], model$data$dataSC))))
+  }
+
+  if(is(model)[1] == "opls"){
+    corr <- abs(t(as.data.frame(cor(model@scoreMN[,PC], model@suppLs[["xModelMN"]]))))
+  }
+
+  if(is(model)[1] == "data.frame"){
+    #no correlation because there's no scores (not a model)
+    corr <- data.frame(matrix(0, nrow = ncol(model), ncol = 1))
+  }
+
+  statistic <- corr
+}
+
+##########Fold change#########
+
+if(stat == "fc"){
+  fc <- foldChange(model = model, optns = optns)
+  statistic <- fc
+}
+
+########external########
+  if(stat == "external"){
+    #add check that there is the correct length of this stat
+    if(nrow(optns$external) == length(id)){
+      statistic <- optns$external
+    }else {stop("length of externally provided stat does not match the number of lipids") }
+  }
+
+########adjusted p-value########
+
+  # if(!"method" %in% names(optns)){
+  #   optns$method <- "bonferroni"
+  # }
+
+  # if(stat == "pval"){
+  #   for(j in 2: length(unique_factors)){
+  #
+  #     df2 <- df[df[,"factor"] %in% c(1, j), ]
+  #
+  #      pval <- data.frame(matrix(NA, nrow = ncol(df)-1, ncol = 1))
+  #      for(i in 1:(ncol(df2)-1)){
+  #        pval[i,]<-kruskal.test(df2[,i], df2[,"factor"])$p.value
+  #       }
+  #
+  #     pvalUnadjusted[,j] <- pval
+  #
+  #     #rescaled and adjusted p-value
+  # for(i in 1:(ncol(df2)-1)){
+  #   pvalRescaled[i,j] <- abs(log10(p.adjust(pvalUnadjusted[i, j], method = optns$method)))
+  # }
+  #   }
+  #
+  #   # Create a mapping between numbers and words, rename pval dataframe columns
+  #   mapping <- setNames(unique(optns$factor), unique(df[,"factor"]))
+  #
+  #   testnames<- as.data.frame(mapping)
+  #   testnames$rowName <- rownames(testnames)
+  #
+  #   for (i in seq_len(nrow(testnames))) {
+  #     col_number <- as.numeric(testnames[i, 2])
+  #     new_col_name <- as.character(testnames[i, 1])
+  #     names(pvalUnadjusted)[col_number] <- new_col_name
+  #     names(pvalRescaled)[col_number] <- new_col_name
+  #   }
+  #
+  #   #if only 2 factors, just need the second column, since first is all NA (control to control)
+  #   if(length(unique_factors) == 2){
+  #     pvalUnadjusted <- as.data.frame(pvalUnadjusted[,2])
+  #     pvalRescaled <- as.data.frame(pvalRescaled[,2])
+  #   }
+  #
+  #   statistic <- pvalRescaled
+  # }
+  #
+
+
+
+#########lipid data frame############
+
+#make class and sc info
+  # id<- colnames(expV[,182:ncol(expV)])
+  # id <- colnames(model)
+  lipids <- id
+  lmc <- strsplit(lipids, "\\(")
+  lmc <- rapply(lmc, function(x) c(x[1], gsub("\\)", "", x[2])), how = "replace")
+
+  r <- list()
+  for (i in 1:length(lmc)) {
+    struc <- strsplit(lmc[[i]][2], "_")[[1]]
+    if (length(struc) == 1 | lmc[[i]][1] == "TAG") {
+      totalCarbon <- gsub("[a-zA-Z\\-]+",
+                          "",
+                          strsplit(struc, ":")[[1]][1])
+      if (lmc[[i]][1] == "TAG") {
+        sideChain <- gsub("[a-zA-Z\\-]+",
+                          "",
+                          strsplit(lmc[[i]][2], "_")[[1]][2])
+      } else {
+        unsat <- gsub("[a-zA-Z\\-]+",
+                      "",
+                      strsplit(struc, ":")[[1]][2])
+        sideChain <- paste0(totalCarbon, ":", unsat)
+      }
+    } else {
+      t <- strsplit(struc, ":")
+      sc <- unlist(lapply(t, function(x)
+        as.numeric(
+          gsub("[a-zA-Z\\-]+",
+               "",
+               x[1])
+        )))
+      unsatSc <- unlist(lapply(t, function(x)
+        as.numeric(
+          gsub("[a-zA-Z\\-]+",
+               "",
+               x[2])
+        )))
+      totalCarbon <- sum(sc)
+      sideChain <- paste0(sc, ":", unsatSc)
+      unsat <- sum(unsatSc)
+    }
+    r[[i]] <- c(lmc[[i]][1], totalCarbon, unsat, sideChain)
+  }
+
+  #find the max number of segments r has been split into for any single entry in the list
+  l <- max(unlist(lapply(r, function(x) length(x))))
+
+  #make all elements of r the length you determined as l with NA's in the empty slots
+  r <- lapply(r, function(x) c(x, rep(NA, l-length(x))))
+
+  #make r into data frame with a col for class, totalCarbon, unsat and sidechain
+  lipidClass <- data.frame(do.call("rbind", r))
+  colnames(lipidClass) <- c("class", "nC", "r", "sc1", "sc2")
+
+ld<-cbind(statistic, id, lipidClass)
+
+#which groups will be plotted
+if("columns_to_plot" %in% names(optns)){
+  columns_to_plot <- optns$columns_to_plot
+} else{columns_to_plot <- colnames(ld)[2:(which(colnames(ld) == "id") - 1)]}
+
+  # Filter the data frame to include only the relevant columns
+  plot_data <- ld[, c("class", "sc1", "id",columns_to_plot)]
+
+  # Reshape the data frame to long format manually
+  plot_data_long <- reshape(plot_data,
+                            idvar = c("class", "sc1", "id"),
+                            varying = list(columns_to_plot),
+                            v.names = c("Value"),
+                            times = columns_to_plot,
+                            direction = "long")
+
+
+  colnames(plot_data_long) <- c("class", "sc1", "id", "Group", "Value")
+
+########### Create the ggplot###########
+  lipidGraph <- ggplot(plot_data_long, aes(x = class,
+                                           y = sc1,
+                                           color = Group)) +
+                geom_jitter(aes(size = abs(Value)),
+                            position = position_jitter(height = 0.1,
+                                                       width = 0.3),
+                            alpha = 0.3) +
+                xlab("Lipid Class") +
+                ylab("Side-Chain Length") +
+                theme_bw() +
+                theme(panel.grid.major = element_line(color = "gray95"),
+                      panel.grid.minor = element_blank(),
+                      legend.position = "none") +
+                scale_color_manual(values = rainbow(length(columns_to_plot)))
+
+
+return(lipidGraph)
+}
+
+
+
--- tests/testthat/test-lipidGraph.R
+++ tests/testthat/test-lipidGraph.R
@@ -1,10 +1,3 @@
-# test_that("multiplication works", {
-#   expect_equal(2 * 2, 4)
-# })
-# # Loading In
-#
-# ## Library packages
-#
 # package_list <- c(
 #   'tidyverse', 'janitor', 'qualV', 'kableExtra', 'fusion', 'readxl', 'ggforce',
 #   'broom', 'ropls', 'fusion', 'gridExtra', 'ggforce', 'kableExtra', 'purrr',
@@ -20,35 +13,24 @@
 #   }
 # }
 #
-# #devtools::load_all("~/Documents/Git/phenological /mva-plots/")
+# # install.packages("devtools")
 # devtools::install_github("phenological/nmr-parser")
-# set.seed(123)
-#
-# ## Data
-#
 #
 # pathToData <- file.path(Sys.getenv()['DATASETS'], "covid19")
 # load(file.path(pathToData, "harvardC2", "datasets", "HUMegaTBL.RDA"))
-#
-#
-# # Setting a backup
-#
 #
 # expV <- megatbl
 # expV <- expV %>%
 #   relocate(cohort, .after = sampleID)
 # resV <- AnnoR
 #
-#
-# ## Cleaning for analysis
-# # removing glutamine and glutamic from ivdr
 # expV %>% select(-c(`Glutamic acid_IVDR`,
 #                    Glutamine_IVDR,
 #                    `1-methylhistidine`,
 #                    `Homocysteine AccQTag`,
 #                    `Cysteine-AccQTag`)) -> expV
 #
-# lipo <- nmr.parser::getLipoTable()
+# #lipo <- nmr.parser::getLipoTable()
 #
 # # create names for publication
 # id <- colnames(expV)[-2]
@@ -104,10 +86,6 @@
 # # refactoring
 # resV$onset <- factor(resV$onset)
 # resV$group <- factor(resV$group)
-#
-#
-# ## Final cohort numbers
-#
 # #check dimensions
 # cat(crayon::bgMagenta("max number of analytes:", length(
 #   c(tagLipo, tagspc, tagAA, tagLip, tagsm, tagTryp)
@@ -133,306 +111,159 @@
 #   resV$cohort
 # )), table(resV$cohort), "\n")))
 #
-# ## Function to format OPLS eruption plot
-#
-#
-# # printing table of most relevant variables
-#
-# formatEruptionTable <- function(eruption) {
-#   # adjusting number format
-#   er <- as.data.frame(eruption@suppLs[["eruptionData"]])
-#   er$pval <- formatC(er$pval, format = "e", 3)
-#   er$pval <- as.numeric(as.character(er$pval))
-#   er$pvalRaw <- as.numeric(as.character(er$pvalRaw))
-#
-#   # printing
-#   tb <- er %>%
-#     rename(Cliffs_Delta = cd, adj.pVal = pval, FoldChange = fc, Correlation = corr, pVal = pvalRaw ) %>%
-#     relocate(id, .before = everything()) %>%
-#     relocate(loadings, .after = id) %>%
-#     arrange(Cliffs_Delta) %>%
-#     mutate_at(3:6, round, 3)
-#
-#   # matching and adding description
-#   nmr.parser::getLipoTable()
-#   idx <- match(tb$id, (lipo$ID))
-#   tb$Description[!is.na(idx)] <- lipo$tag[idx[!is.na(idx)]]
-#   tb$Description[is.na(idx)] <- tb$id[is.na(idx)]
-#
-#   tb <- tb %>%
-#     filter(adj.pVal < 0.05) %>%
-#     select(Description, Cliffs_Delta, pVal, adj.pVal, FoldChange, Correlation) %>%
-#     mutate(Cliffs_Delta = format(round(Cliffs_Delta, 2), nsmall = 2),
-#            `Adj. p-value` = signif(adj.pVal, digits = 3),
-#            Correlation = format(round(Correlation, 3))) %>%
-#     rename(`Cliff's Delta` = Cliffs_Delta)
-# }
-#
-# reshapeTable <- function(table, n) {
-#   nr <- ceiling(nrow(table) / n)
-#   while (nrow(table) < nr * n) {
-#     table <- rbind(table, rep("", ncol(table)))
-#   }
-#   new <- table[1:nr,]
-#   for (i in 2:n) {
-#     beg <- (i - 1) * nr + 1
-#     end <- i * nr
-#     chk <- table[beg:end,]
-#     colnames(chk) <- paste0(colnames(table), "..", i)
-#     new <- cbind(new, chk)
-#   }
-#   return(new)
-# }
-#
-#
-#
-#
-# #names of lipids appears as such: "TAG(60:10_FA22:6)" "TAG(60:11_FA22:5)" "TAG(60:11_FA22:6)" "PS(20:0_20:5)"     "PS(20:0_22:4)"     "PS(20:0_22:5)"     "PS(20:0_22:6)"     "SM(14:0)"
-# #[473] "SM(16:0)"          "SM(18:0)"          "SM(18:1)" "PG(20:0_18:3)"     "PG(20:0_20:1)"     "PG(20:0_20:2)"     "PG(20:0_20:3)"     "PG(20:0_20:4)"     "PG(20:0_22:4)"     "PG(20:0_22:5)"
-# #[409] "PG(20:0_22:6)"     "PI(14:0_18:1)"     "PI(16:0_16:0)"     "PI(16:0_16:1)"     "PI(16:0_18:0)"     "PI(16:0_18:1)"     "PI(16:0_18:2)"     "PI(16:0_18:3)"
-# #[417] "PI(16:0_20:1)"     "PI(16:0_20:2)"  and so on
-#
-# #want to create lipid classes. first, split  "CE(14:0)"  into "CE"  and  "14:0)"
-# lmc <- strsplit(tagLip[-1], "\\(")
-# lmc <- strsplit(tagLip[-1], "\\(")
-#
-# #then remove last bracket to get "CE"  and  "14:0"
-# lmc <- rapply(lmc, function(x) c(x[1], gsub("\\)", "", x[2])), how = "replace")
-# lmc <- rapply(lmc, function(x) c(x[1], gsub("\\)", "", x[2])), how = "replace")
-# #need the length of struc and the name from lmc
-# #store just the numbers
-# struc <- strsplit(lmc[[1]][2], "_")[[1]]
-#
-# struc <- list()
-# for (i in 1:length(lmc)) {
-#   struc[[i]] <- strsplit(lmc[[i]][2], "_")[[1]]}
-#
-# #the alphbetical part
-# lmc[[1]][1]
-# #depending on the length of struc and what the alphabetical part is, do something
-# #length struc == 1 OR alphabetical is TAG
-#
-#
-#
-# lmc[[1]]
-# #[1] "CE"   "14:0"
-#
-# totalCarbon <- gsub("[a-zA-Z\\-]+",
-#                     "",
-#                     strsplit(struc[[1]], ":")[[1]][1])
-# #14
-#
-# unsat <- gsub("[a-zA-Z\\-]+",
-#               "",
-#               strsplit(struc[[1]], ":")[[1]][2])
-# #0
-#
-# sideChain <- paste0(totalCarbon, ":", unsat)
-# #"14:0"
-#
-# lmc[[640]]
-# #[1] "TAG"         "51:3_FA17:0"
-#
-# totalCarbon <- gsub("[a-zA-Z\\-]+",
-#                     "",
-#                     strsplit(struc[[640]], ":")[[1]][1])
-#
-# sideChain <- gsub("[a-zA-Z\\-]+",
-#                   "",
-#                   strsplit(lmc[[640]][2], "_")[[1]][2])
-#
-#
-# lmc[[123]]
-# #"HCER"       "d18:0_18:0"
-# length(struc[[123]])
-# #2
-#
-# #separate into "d18", "0", "18" and "0"
-#   t <- strsplit(struc[[123]], ":")
-#
-# #get numbers before ":" and make numeric
-#   sc <- unlist(lapply(t, function(x)
-#     as.numeric(
-#       gsub("[a-zA-Z\\-]+",
-#            "",
-#            x[1])
-#     )))
-#
-# #get the numbers after ":" and make numeric
-#   unsatSc <- unlist(lapply(t, function(x)
-#     as.numeric(
-#       gsub("[a-zA-Z\\-]+",
-#            "",
-#            x[2])
-#     )))
-#
-#
-#   totalCarbon <- sum(sc)
-#
-#   sideChain <- paste0(sc, ":", unsatSc)
-#
-#   unsat <- sum(unsatSc)
-#
-# function(lipids, measurements, stat, group, optns = list()){
-#
-#   if(is(lipids[1] == "character")){
-#
-#   }
-#
-#   lmc <- strsplit(lipids, "\\(")
-#   lmc <- rapply(lmc, function(x) c(x[1], gsub("\\)", "", x[2])), how = "replace")
-#   lmc <- lipids
-#
-#   #create lipid classes table
-#
-#   r <- list()
-#   for (i in 1:length(lmc)) {
-#     struc <- strsplit(lmc[[i]][2], "_")[[1]]
-#     if (length(struc) == 1 | lmc[[i]][1] == "TAG") {
-#       totalCarbon <- gsub("[a-zA-Z\\-]+",
-#                           "",
-#                           strsplit(struc, ":")[[1]][1])
-#       if (lmc[[i]][1] == "TAG") {
-#         sideChain <- gsub("[a-zA-Z\\-]+",
-#                           "",
-#                           strsplit(lmc[[i]][2], "_")[[1]][2])
-#       } else {
-#         unsat <- gsub("[a-zA-Z\\-]+",
-#                       "",
-#                       strsplit(struc, ":")[[1]][2])
-#         sideChain <- paste0(totalCarbon, ":", unsat)
-#       }
-#     } else {
-#       t <- strsplit(struc, ":")
-#       sc <- unlist(lapply(t, function(x)
-#         as.numeric(
-#           gsub("[a-zA-Z\\-]+",
-#                "",
-#                x[1])
-#         )))
-#       unsatSc <- unlist(lapply(t, function(x)
-#         as.numeric(
-#           gsub("[a-zA-Z\\-]+",
-#                "",
-#                x[2])
-#         )))
-#       totalCarbon <- sum(sc)
-#       sideChain <- paste0(sc, ":", unsatSc)
-#       unsat <- sum(unsatSc)
-#     }
-#     r[[i]] <- c(lmc[[i]][1], totalCarbon, unsat, sideChain)
-#   }
-#
-#   #find the max number of segments r has been split into for any single entry in the list
-#   l <- max(unlist(lapply(r, function(x) length(x))))
-#
-#   #make all elements of r the length you determined as l with NA's in the empty slots
-#   r <- lapply(r, function(x) c(x, rep(NA, l-length(x))))
-#
-#   #make r into data frame with a col for class, totalCarbon, unsat and sidechain
-#   lipidClass <- data.frame(do.call("rbind", r))
-#
-#   colnames(lipidClass) <- c("class", "nC", "r", "sc1", "sc2")
-# }
-#
-#
-#
-# #make desired statistics for size based on the chosen groups for color
-#
-#
-# # statT_filtered <- statT[statT$`p-Value adj. > 1.3,] %>% arrange(`MIS-C)
-# statT_filtered <- statT[statT$`p-Value adj. > 1.3,] %>% arrange(`MIS-C`)
-# keep <- statT_filtered$var
-#
-# # getting proper names
-# lipo <- nmr.parser::getLipoTable()
-#
-# idx <- match(tolower(statT_filtered$var), tolower(lipo$ID))
-# statT_filtered$var[!is.na(idx)] <- lipo$tag[idx[!is.na(idx)]]
-#
-# bLip <- statT_filtered[statT_filtered$var %in% tagLip,]
-# idx <- match(bLip$var, tagLip)
-# bLip$class <- lipidClass$class[idx]
-# bLip$sc <- lipidClass$sc1[idx]
-#
-# ########cliffs delta##########
-# cd <- cliffsDelta(model = model, optns = optns)
-#
-# ##########Fold change#########
-# fc <- foldChange(model = model, optns = optns)
-#
-# idx<- which(df[,"factor"] == 1)
-# control <- df[idx,]
-# treatment <- df[-idx,]
-#
-# #logmeans
-# c <- log2(apply(X = control, MARGIN = 2, FUN = mean))
-# t <- log2(apply(X = treatment, MARGIN = 2, FUN = mean))
-#
-# #log2 fold change
-# log2fc <- as.data.frame(c-t)
-# log2fc <- as.data.frame(log2fc[1:(nrow(log2fc)-1),])
-#
-# ########adjusted p-value########
-# pval<-list()
-# for(i in 1:(ncol(df)-1)){
-#   pval[[i]]<-kruskal.test(df[,i], df[,"factor"])$p.value
-# }
-#
-# unlist(pval)
-# pvalUnadjusted <- t(as.data.frame(pval))
-# pvalAdjusted <- p.adjust(pval, method = method)
-# pvalRescaled <- abs(log10(pvalAdjusted))
-# pvalRescaled <- as.data.frame(pvalRescaled)
-#
-# #eruption data frame
-# ed<-cbind(cd, fc, pvalRescaled, pvalUnadjusted)
-#
-# colnames(ed)<-c("cd", "fc", "pval", "pvalRaw")
-#
-# #make the graph
-# plip1 <- ggplot(bLip, aes(x = class, y = sc)) +
-#   # geom_point(size = bLip$`MIS-C (Kids)`, color = "firebrick3") +
-#   # geom_point(size = bLip$`Controls (Kids)`, color = "dodgerblue")
-#   geom_jitter(aes(size = abs(MIS-C)),
-#               position = position_jitter(height = 0.1, width = 0.3),
-#               alpha = 0.3,
-#               color = "dodgerblue") +
-#   geom_jitter(aes(size = abs(Covid-19 (Children))),
-#               position = position_jitter(height = 0.1, width = 0.3),
-#               alpha = 0.3,
-#               color = "firebrick3") +
-#   scale_size_continuous(range = c(0, 5),
-#                         trans = scales::exp_trans(base = 1.3),
-#                         breaks = c(1, 1.5, 2)) +
-#   xlab("Lipid Class") +
-#   ylab("Side-Chain Length") +
-#   theme_bw() +
-#   theme(panel.grid.major = element_line(color = "gray95"),
-#         panel.grid.minor = element_blank(),
-#         legend.position = "none")
-#
-# plip2 <- ggplot(bLip[bLip$class == "TAG",], aes(x = class, y = sc)) +
-#   # geom_point(size = bLip$`MIS-C (Kids)`, color = "firebrick3") +
-#   # geom_point(size = bLip$`Controls (Kids)`, color = "dodgerblue")
-#   geom_jitter(aes(size = abs(MIS-C)),
-#               position = position_jitter(height = 0.1, width = 0.3),
-#               alpha = 0.3,
-#               color = "dodgerblue") +
-#   geom_jitter(aes(size = abs(Covid-19 (Children))),
-#               position = position_jitter(height = 0.1, width = 0.3),
-#               alpha = 0.3,
-#               color = "firebrick3") +
-#   scale_size_continuous(range = c(0, 5),
-#                         trans = scales::exp_trans(base = 1.3),
-#                         breaks = c(1, 1.5, 2)) +
-#   xlab("") +
-#   theme_bw() +
-#   theme(panel.grid.major = element_line(color = "gray95"),
-#         panel.grid.minor = element_blank(),
-#         legend.position = "none",
-#         # axis.text.y=element_blank(),
-#         axis.title.y=element_blank())
 
 
+test_that("You can use a data frame", {
+
+  #don't specify control, expect warning
+  expect_warning(object = lipidGraph(model = lipidData,
+                                     stat = "fc",
+                                     optns = list(factor = (lipidMetadata$Class))),
+                 regexp = "No control specified in optns for factor. The first entry was set as the control")
+
+  #do specify control
+  lg<- lipidGraph(model = lipidData,
+                  stat = "fc",
+                  optns = list(factor = (lipidMetadata$Class),
+                               control = "Control"))
+
+  expect_equal(object = unique(lg[["data"]][["Group"]])[1], expected = "COVID")
+  expect_equal(object = unique(lg[["data"]][["Group"]])[2], expected = "MISC")
+
+
+  #specify which group want graphed
+  lg<- lipidGraph(model = lipidData,
+                  stat = "fc",
+                  optns = list(factor = (lipidMetadata$Class),
+                               control = "Control",
+                               columns_to_plot = "MISC"))
+  expect_equal(object = unique(lg[["data"]][["Group"]])[1], expected = "MISC")
+  expect_equal(object = unique(lg[["data"]][["Group"]])[2], expected = as.character(NA))
+
+  #if change control, do the correct groups show up? use cd instead of fc
+
+  lg<- lipidGraph(model = lipidData,
+                  stat = "cd",
+                  optns = list(factor = (lipidMetadata$Class),
+                               control = "MISC"))
+  expect_length(object = unique(lg[["data"]][["Group"]]), n = 2)
+  expect_contains(object = lg[["data"]][["Group"]], expected = "Control")
+  expect_contains(object = lg[["data"]][["Group"]], expected = "COVID")
+
+})
+
+test_that("NA in factor will throw error",{
+  #change one lipidMetadata Class entry to NA
+  lipidMetadata2 <- lipidMetadata
+  lipidMetadata2[1,"Class"] <- NA
+
+  expect_error(object = lipidGraph(model = lipidData,
+                                   stat = "cd",
+                                   optns = list(factor = (lipidMetadata2$Class),
+                                                control = "Control")),
+               regexp = "One of your factors is NA, please change this before running lipidGraph")
+
+})
+
+test_that("external stat can be supplied, including with more than one group to be graphed",{
+
+  #supply external stat
+
+   ########adjusted p-value########
+   model <- lipidData
+
+   optns <- list(factor = as.vector(lipidMetadata$Class), control = "Control", method = "fdr")
+
+   id <- colnames(model)
+   df <- model
+
+     # #empty df
+      pvalUnadjusted <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+      pvalRescaled <- data.frame(matrix(NA, nrow = ncol(df), ncol = 1))
+
+     df[,"factor"] <- as.numeric(relevel(as.factor(optns$factor), ref = optns$control))
+     unique_factors <- unique(df[,"factor"])
+
+     for(j in 2: length(unique_factors)){
+
+       df2 <- df[df[,"factor"] %in% c(1, j), ]
+
+       pval <- data.frame(matrix(NA, nrow = ncol(df)-1, ncol = 1))
+       for(i in 1:(ncol(df2)-1)){
+         pval[i,]<-kruskal.test(df2[,i], df2[,"factor"])$p.value
+       }
+
+       pvalUnadjusted[,j] <- pval
+
+       #rescaled and adjusted p-value
+       for(i in 1:(ncol(df2)-1)){
+         pvalRescaled[i,j] <- abs(log10(p.adjust(pvalUnadjusted[i, j], method = optns$method)))
+       }
+     }
+
+     # Create a mapping between numbers and words, rename pval dataframe columns
+     mapping <- setNames(unique(optns$factor), unique(df[,"factor"]))
+
+     testnames<- as.data.frame(mapping)
+     testnames$rowName <- rownames(testnames)
+
+     for (i in seq_len(nrow(testnames))) {
+       col_number <- as.numeric(testnames[i, 2])
+       new_col_name <- as.character(testnames[i, 1])
+       names(pvalUnadjusted)[col_number] <- new_col_name
+       names(pvalRescaled)[col_number] <- new_col_name
+     }
+
+   lg<- lipidGraph(model = lipidData,
+                   stat = "external",
+                   optns = list(factor = (lipidMetadata$Class),
+                                control = "Control",
+                                external = pvalRescaled))
+
+   expect_length(object = unique(lg[["data"]][["Group"]]), n = 2)
+   expect_contains(object = lg[["data"]][["Group"]], expected = "COVID")
+   expect_contains(object = lg[["data"]][["Group"]], expected = "MISC")
+
+})
+
+   #use PCA model
+
+  test_that("You can use PCA object", {
+    #lipidData <- readRDS(system.file("extdata", "lipidData.rds", package = "mva.plots"))
+    #lipidMetadata <- readRDS(system.file("extdata", "lipidMetadata.rds", package = "mva.plots"))
+
+    pca<- PCA(data = lipidData, plot = FALSE, rank =3)
+
+    lg<- lipidGraph(model = pca,
+                    stat = "cd",
+                    optns = list(factor = (lipidMetadata$Class),
+                                 control = "Control"))
+
+    #did the class, side chain and id turn out in the correct format? Use the first lipid to test
+    expect_equal(object = lg[["data"]][["class"]][1], expected = "CE")
+    expect_equal(object = lg[["data"]][["sc1"]][1], expected = "14:0")
+    expect_equal(object = lg[["data"]][["id"]][1], expected = "CE(14:0)")
+  })
+
+   #use oplsda model
+test_that("You can use oplsda object", {
+  lipidData <- readRDS(system.file("extdata", "lipidData.rds", package = "mva.plots"))
+  lipidMetadata <- readRDS(system.file("extdata", "lipidMetadata.rds", package = "mva.plots"))
+  op<- oplsda(X = lipidData, Y = lipidMetadata$Class, type = "PLS")
+  lg<- lipidGraph(model = op,
+                  stat = "cd",
+                  optns = list(factor = (lipidMetadata$Class),
+                               control = "Control"))
+
+  # expect_gt(length(lg[["data"]][["class"]]), 0)
+  # expect_type(object = lg[["data"]][["class"]], type = "character")
+  # expect_type(object = lg[["data"]][["sc1"]], type = "character")
+  # expect_type(object = lg[["data"]][["Value"]], type = "double")
+  #
+  #did the class, side chain and id turn out in the correct format? Use the first lipid to test
+  expect_equal(object = lg[["data"]][["class"]][1], expected = "CE")
+  expect_equal(object = lg[["data"]][["sc1"]][1], expected = "14:0")
+  expect_equal(object = lg[["data"]][["id"]][1], expected = "CE(14:0)")
+
+})
 
